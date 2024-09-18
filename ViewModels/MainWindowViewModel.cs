20bbfd11ba03c5a11ba1d21a6927ef363c611742
Piotr S. <piotr.sasadeusz@gmail.com>
2024-09-18 20:25:27 +0200

    copilot - refactor

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,192 +1,187 @@
-﻿namespace ApiTester.ViewModels;
-
-using System;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Net.Http;
-using System.Reactive;
-using System.Reactive.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using ApiTester.Builders;
-using ApiTester.Models;
-using Newtonsoft.Json;
-using ReactiveUI;
-
-public partial class MainWindowViewModel : ViewModelBase
+﻿namespace ApiTester.ViewModels
 {
-    public ObservableCollection<string> HttpMethods { get; } = new ObservableCollection<string> { "GET", "POST", "PUT", "DELETE" };
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Reactive;
+    using System.Reactive.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using ApiTester.Builders;
+    using ApiTester.Models;
+    using Newtonsoft.Json;
+    using ReactiveUI;
 
-    private string _selectedHttpMethod = "GET";
-    public string HttpMethod
+    public partial class MainWindowViewModel : ViewModelBase
     {
-        get => _selectedHttpMethod;
-        set => this.RaiseAndSetIfChanged(ref _selectedHttpMethod, value);
-    }
+        public ObservableCollection<string> HttpMethods { get; } = new ObservableCollection<string> { "GET", "POST", "PUT", "DELETE" };
 
-    private string _selectedContentType = "application/json";
-
-    public string ContentType
-    {
-        get => _selectedContentType;
-        set => this.RaiseAndSetIfChanged(ref _selectedContentType, value);
-    }
-
-    public bool AppJsonEnabled
-    {
-        get => _selectedContentType == "application/json";
-        set => this.RaiseAndSetIfChanged(ref _selectedContentType, "application/json");
-    }
-
-    public bool AppXmlEnabled
-    {
-        get => _selectedContentType == "application/xml";
-        set => this.RaiseAndSetIfChanged(ref _selectedContentType, "application/xml");
-    }
-
-    public bool TextPlainEnabled
-    {
-        get => _selectedContentType == "text/plain";
-        set => this.RaiseAndSetIfChanged(ref _selectedContentType, "text/plain");
-    }
-
-    private string _url;
-    public string Url
-    {
-        get => _url;
-        set => this.RaiseAndSetIfChanged(ref _url, value);
-    }
-
-    private string _requestBody = string.Empty;
-    public string RequestBody
-    {
-        get => _requestBody;
-        set => this.RaiseAndSetIfChanged(ref _requestBody, value);
-    }
-
-    private string _validationStatus;
-
-    public string ValidationStatus
-    {
-        get => _validationStatus;
-        set => this.RaiseAndSetIfChanged(ref _validationStatus, value);
-    }
-
-    private int _messageCount = 1;
-    public int MessageCount
-    {
-        get => _messageCount;
-        set => this.RaiseAndSetIfChanged(ref _messageCount, value);
-    }
-
-    private bool _sendInParallel = false;
-    public bool SendInParallel
-    {
-        get => _sendInParallel;
-        set => this.RaiseAndSetIfChanged(ref _sendInParallel, value);
-    }
-    private int _numberOfThreads = 1;
-
-    public int NumberOfThreads
-    {
-        get => _numberOfThreads;
-        set => this.RaiseAndSetIfChanged(ref _numberOfThreads, value);
-    }
-
-    public ReactiveCommand<Unit, Unit> SendCommand { get; }
-    public ReactiveCommand<Unit, Unit> FormatCommand { get; }
-
-    public MainWindowViewModel()
-    {
-        SendCommand = ReactiveCommand.CreateFromTask(SendRequestsInParallel);
-        FormatCommand = ReactiveCommand.Create(Format);
-        HttpRequestResults = new ObservableCollection<HttpRequestResult>();
-    }
-
-    public ObservableCollection<HttpRequestResult> HttpRequestResults { get; }
-
-
-    //sends request in parallel, number o threads is defined by NumberOfThreads, total number of requests is defined by MessageCount
-    //each request is sent to the same URL with the same body and headers
-    //each request is sent with a unique request number
-    //response is stored in HttpRequestResults.
-    // request message is built using MessageBuilder.BuildRequest method
-
-    public async Task SendRequestsInParallel()
-    {
-        Format();
-
-        var semaphore = new SemaphoreSlim(NumberOfThreads);
-        var tasks = Enumerable.Range(0, MessageCount).Select(async i =>
+        private string _selectedHttpMethod = "GET";
+        public string HttpMethod
         {
-            await semaphore.WaitAsync();
-            try
+            get => _selectedHttpMethod;
+            set => this.RaiseAndSetIfChanged(ref _selectedHttpMethod, value);
+        }
+
+        private string _selectedContentType = "application/json";
+        public string ContentType
+        {
+            get => _selectedContentType;
+            set => this.RaiseAndSetIfChanged(ref _selectedContentType, value);
+        }
+
+        public bool AppJsonEnabled
+        {
+            get => _selectedContentType == "application/json";
+            set => this.RaiseAndSetIfChanged(ref _selectedContentType, "application/json");
+        }
+
+        public bool AppXmlEnabled
+        {
+            get => _selectedContentType == "application/xml";
+            set => this.RaiseAndSetIfChanged(ref _selectedContentType, "application/xml");
+        }
+
+        public bool TextPlainEnabled
+        {
+            get => _selectedContentType == "text/plain";
+            set => this.RaiseAndSetIfChanged(ref _selectedContentType, "text/plain");
+        }
+
+        private string _url;
+        public string Url
+        {
+            get => _url;
+            set => this.RaiseAndSetIfChanged(ref _url, value);
+        }
+
+        private string _requestBody = string.Empty;
+        public string RequestBody
+        {
+            get => _requestBody;
+            set => this.RaiseAndSetIfChanged(ref _requestBody, value);
+        }
+
+        private string _validationStatus;
+        public string ValidationStatus
+        {
+            get => _validationStatus;
+            set => this.RaiseAndSetIfChanged(ref _validationStatus, value);
+        }
+
+        private int _messageCount = 1;
+        public int MessageCount
+        {
+            get => _messageCount;
+            set => this.RaiseAndSetIfChanged(ref _messageCount, value);
+        }
+
+        private bool _sendInParallel = false;
+        public bool SendInParallel
+        {
+            get => _sendInParallel;
+            set => this.RaiseAndSetIfChanged(ref _sendInParallel, value);
+        }
+
+        private int _numberOfThreads = 1;
+        public int NumberOfThreads
+        {
+            get => _numberOfThreads;
+            set => this.RaiseAndSetIfChanged(ref _numberOfThreads, value);
+        }
+
+        public ReactiveCommand<Unit, Unit> SendCommand { get; }
+        public ReactiveCommand<Unit, Unit> FormatCommand { get; }
+
+        public ObservableCollection<HttpRequestResult> HttpRequestResults { get; }
+
+        public MainWindowViewModel()
+        {
+            SendCommand = ReactiveCommand.CreateFromTask(SendRequestsInParallel);
+            FormatCommand = ReactiveCommand.Create(Format);
+            HttpRequestResults = new ObservableCollection<HttpRequestResult>();
+        }
+
+        public async Task SendRequestsInParallel()
+        {
+            Format();
+
+            var semaphore = new SemaphoreSlim(NumberOfThreads);
+            var tasks = Enumerable.Range(0, MessageCount).Select(async i =>
             {
-                using (var client = new HttpClient())
+                await semaphore.WaitAsync();
+                try
                 {
-                    var request = MessageBuilder.BuildRequest(Url, HttpMethod, ContentType, RequestBody);
-                    var stopwatch = System.Diagnostics.Stopwatch.StartNew();
-                    var response = await client.SendAsync(request);
-                    stopwatch.Stop();
-
-                    var result = new HttpRequestResult
-                    {
-                        RequestNumber = i + 1,
-                        ResponseCode = (int)response.StatusCode,
-                        ResponseContent = (await response.Content.ReadAsStringAsync())?.Substring(0, 100) ?? string.Empty,
-                        RequestDuration = stopwatch.Elapsed
-                    };
-
+                    var result = await SendHttpRequestAsync(i + 1);
                     lock (HttpRequestResults)
                     {
                         HttpRequestResults.Add(result);
                     }
                 }
+                catch (Exception ex)
+                {
+                    var errResult = new HttpRequestResult
+                    {
+                        RequestNumber = i + 1,
+                        ResponseCode = 0,
+                        ResponseContent = ex.Message,
+                        RequestDuration = TimeSpan.Zero
+                    };
+                    lock (HttpRequestResults)
+                    {
+                        HttpRequestResults.Add(errResult);
+                    }
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+            });
+
+            await Task.WhenAll(tasks);
+        }
+
+        private async Task<HttpRequestResult> SendHttpRequestAsync(int requestNumber)
+        {
+            using (var client = new HttpClient())
+            {
+                var request = MessageBuilder.BuildRequest(Url, HttpMethod, ContentType, RequestBody);
+                var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+                var response = await client.SendAsync(request);
+                stopwatch.Stop();
+
+                return new HttpRequestResult
+                {
+                    RequestNumber = requestNumber,
+                    ResponseCode = (int)response.StatusCode,
+                    ResponseContent = (await response.Content.ReadAsStringAsync())?.Substring(0, 100) ?? string.Empty,
+                    RequestDuration = stopwatch.Elapsed
+                };
+            }
+        }
+
+        private void Format()
+        {
+            ValidationStatus = string.Empty;
+            try
+            {
+                RequestBody = FormatJson(RequestBody);
             }
             catch (Exception ex)
             {
-                var errResult = new HttpRequestResult
-                {
-                    RequestNumber = i + 1,
-                    ResponseCode = 0,
-                    ResponseContent = ex.Message,
-                    RequestDuration = TimeSpan.Zero
-                };
-                lock (HttpRequestResults)
-                {
-                    HttpRequestResults.Add(errResult);
-                }
+                ValidationStatus = ex.Message;
             }
-            finally
+        }
+
+        private static string FormatJson(string json)
+        {
+            if (string.IsNullOrWhiteSpace(json))
             {
-                semaphore.Release();
+                return string.Empty;
             }
-        });
-
-        await Task.WhenAll(tasks);
-    }
-
-    private void Format()
-    {
-        ValidationStatus = string.Empty;
-        try
-        {
-            RequestBody = FormatJson(RequestBody);
+            object? parsedJson = JsonConvert.DeserializeObject(json);
+            return JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
         }
-        catch (Exception ex)
-        {
-            ValidationStatus = ex.Message;
-        }
-    }
-
-    private static string FormatJson(string json)
-    {
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return string.Empty;
-        }
-        object? parsedJson = JsonConvert.DeserializeObject(json);
-
-        return JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
     }
 }
